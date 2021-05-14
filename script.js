5e28c33d3e1ddfad7e6178a3a17812bd339be49c
SpencerM24 <>
2021-05-14 16:28:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,98 @@
+//Sharon
+letsharonvariable= 4;
+console.log('Sharon's birthday countdown');
+let days ='4'
+console.log ("Happy Birthday 61")
+
+
+// Dan
+let dinner = 'leftovers';
+console.log('I am having ' + dinner + ' for dinner tonight');
+dinner = 'Ramen';
+console.log('A few days ago I had ' + dinner + ' for dinner')
+
+
+//Manny Variable
+let mannyvariable = 15;
+console.log("Manny's birthday is December " +mannyvariable);
+mannyvariable= "The Boys";
+console.log("Dan Wiet's Show is the " +mannyvariable);
+
+let ninasvariable = 5;
+console.log(typeof ninasvariable);
+console.log("Nina's number is: " + ninasvariable);
+ninasvariable = "Tran";
+console.log(typeof ninasvariable);
+console.log("My full name is Nina " + ninasvariable);
+
+
+let johnarray = ['j','o','h','n'];
+let johnvariable1 = johnarray[0];
+let johnvariable2 = johnarray[1];
+let johnvariable3 = johnarray[2]];
+let johnvariable4 = johnarray[3];
+console.log("John's name but scrambled " + johnvariable4 + johnvariable3 + johnvariable2 + johnvariable1 );
+
+//chasitysvariable
+let chasitysvariable = 5;
+console.log("Chasity's birthdate is the: " + chasitysvariable);
+chasitysvariable = "February";
+console.log(typeof chasitysvariable);
+console.log("My birthdate is " + chasitysvariable);
+//chasitysvariable
+
+let shamekavariable = 4;
+console.log (My birthday is + shamekavariable)
+console.log("Happy Birthday")
+
+
+//jasmine//
+let jasminesvariable = 24;
+console.log("Jasmine's number is: " + jasminesvariable);
+jasminesvariable = "Yellow";
+console.log("Jasmine's favorite color is" + jasminesvariable);
+
+
+// alexis //
+let alexis = 'I am tired';
+console.log (alexis);
+alexis = 'Pizza';
+console.log("Alexis ate " + alexis);
+// make sure you have a space after the word ate, or else it will print out Alexis atePizza instead of Alexis ate Pizza
+
+
+//  Billy
+let billyAge = 60;
+      console.log (billyAge +" Is old enough");
+    billyAge = "Old Enough to Know better :)";
+      console.log (billyAge);
+
+
+let nikki = "Watch out";
+console.log(nikki); 
+nikki = "ennbee9";
+console.log("my twitch name is  " + nikki +". No content, I just watch");
+
+
+let goofy = 7;
+console.log (typeof goofy);
+goofy = "Nickname";
+console.log()
+
+let shedrickvariable = 6;
+console.log(7);
+shedrickvariable ="Crockett";
+console.log("My full name is Shedrick " + shedrickvariable);
+
+
+
+let cjword = 100
+console.log();
+
+let nicolesvariable = 13;
+console.log("Nicole's number is: " + nicolesvariable);
+nicolesvariable = "wilson";
+console.log("My full name is Nicole ' + nicolesvariable);
+// the way you have it now will display 'My full name is Nicole Wilsonwilson'
+// console.log('My full name is Nicole ' + nicolesvariable)
+
